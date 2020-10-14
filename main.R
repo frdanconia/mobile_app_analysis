d629c7345df5b63ce5d9f0bf5239b981b577ca6b
frdanconia <60359377+frdanconia@users.noreply.github.com>
2020-10-14 15:01:33 +0200

    update

--- main.R
+++ main.R
@@ -1,20 +1,23 @@
 library(vroom)
 
 klienci <- vroom("data/klienci.csv")
-klienci[,1] <- c()
+session_geo <- vroom("data/session_geo.csv")
+session_info <- vroom("data/session_info.csv")
+
+klienci[, 1] <- c()
 
 length(unique(klienci$klient_id))
 length(unique(klienci$klient_id[klienci$czy_w_bazie_klientow == 1]))
 #Zakladajac, że baza danych zawiera wszystkich klientow firmy, firma ta ma obecnie 4766 klientów którzy znajduja sie przynajmniej w 2 bazach danych firmy oraz 6000 klientów obecnych tylko w analizowanej bazie
 
-klienci_w_bazie <- klienci[klienci$czy_w_bazie_klientow == 1,]
-sum(klienci_w_bazie$czy_kupil)/nrow(klienci_w_bazie)
+klienci_w_bazie <- klienci[klienci$czy_w_bazie_klientow == 1, ]
+sum(klienci_w_bazie$czy_kupil) / nrow(klienci_w_bazie)
 #Sposrod 4766 klientów którzy znajduja sie przynajmniej w 2 bazach danych firmy  18% klientow kupilo uslugi premium
 
-sum(klienci[klienci$czy_w_bazie_klientow == 0,]$czy_kupil)/nrow(klienci[klienci$czy_w_bazie_klientow == 0,])
+sum(klienci[klienci$czy_w_bazie_klientow == 0, ]$czy_kupil) / nrow(klienci[klienci$czy_w_bazie_klientow == 0, ])
 #Sposrod 1234 klientów którzy znajduja sie tylko w tej jednej bazie firmy 16.85% klientow kupilo uslugi premium
 
-sum(klienci$czy_kupil)/nrow(klienci)
+sum(klienci$czy_kupil) / nrow(klienci)
 #Sposrod 6000 klientów którzy znajduja sie przynajmniej w 2 bazach danych firmy  17.8% klientow kupilo uslugi premium
 
 table(klienci$wyksztalcenie[klienci$czy_kupil == 1])
@@ -22,13 +25,30 @@ mean(klienci$wiek[!is.na(klienci$wiek)])
 median(klienci$wiek[!is.na(klienci$wiek)])
 #To czy model biznesowy trafia do osob wyksztalconych zalezy od struktury wyksztalcenia w populacji
 #Nie ma ani jednej obserwacji z wyksztalceniem zasadniczym zawodowym, wiec trzeba byloby sie zastanowic czy jest ono kodowane jako podstawowe czy srednie razem z wyksztalceniem srednim zawodowym
-#Biorac jednak pod uwage ze sredni wiek klientow wynosi 50,87 lat a mediana 51 lat to fakt, że w populacji klientow, ktorzy wykupili uslugi 
-#premium znajduje sie o ponad 40% wiecej osob z wyksztalceniem wyzszym niz podstawowym nalezy uznac za sukces gdyz w populacji Polski osoby 
-#w tym wieku kilkukrotnie czesciej maja wyksztalcenie zasadnicze zawodowe 
-#Jezeli jednak model biznesowy uwzglednial kierowanie sie przede wszystkim do osob z wyksztalceniem wyzszym to  
-#należaloby sie zastanowic czy satysfakcjonuje nas fakt, że mamy dokladnie tyle samo klientow z wyksztalceniem srednim i wyzszym 
+#Biorac jednak pod uwage ze sredni wiek klientow wynosi 50,87 lat a mediana 51 lat to fakt, że w populacji klientow, ktorzy wykupili uslugi
+#premium znajduje sie o ponad 40% wiecej osob z wyksztalceniem wyzszym niz podstawowym nalezy uznac za sukces gdyz w populacji Polski osoby
+#w tym wieku kilkukrotnie czesciej maja wyksztalcenie zasadnicze zawodowe
+#Jezeli jednak model biznesowy uwzglednial kierowanie sie przede wszystkim do osob z wyksztalceniem wyzszym to
+#należaloby sie zastanowic czy satysfakcjonuje nas fakt, że mamy dokladnie tyle samo klientow z wyksztalceniem srednim i wyzszym
 
 
-klienci$wynagrodzenie
+salary <-
+  data.frame(klient_id = klienci$klient_id,
+             wynagrodzenie = klienci$wynagrodzenie)
 
-klienci$
+for (i in 1:length(salary$klient_id)) {
+  salary$nsessions[i] <-
+    sum(session_info$klient_id == salary$klient_id[i])
+}
+
+for (i in 1:length(salary$klient_id)) {
+  salary$session_length_mean[i] <- mean(session_info$dlugosc_sesji_min[session_info$klient_id == salary$klient_id[i]])
+  salary$session_length_median[i] <- median(session_info$dlugosc_sesji_min[session_info$klient_id == salary$klient_id[i]])
+}
+
+plot(salary$nsessions,salary$wynagrodzenie)
+library(tidyverse)
+ggplot(salary,aes(x=nsessions,y=wynagrodzenie)) + geom_point() + geom_smooth(method="loess", se=F)
+#ciezko stwierdzic na ile parametr nsessions odzwierciedla czestosc korzystania z aplikacji 
+
+plot(salary$)
